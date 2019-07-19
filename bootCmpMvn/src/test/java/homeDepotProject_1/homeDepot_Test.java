a7d1c37247b3ca6c44bc0f2faa67ac1dcfa574d7
Mesfin Kassa <Mesfin Kassa@DESKTOP-57TO61H>
2019-07-19 19:13:48 -0400

    commit3

--- bootCmpMvn/src/test/java/homeDepotProject_1/homeDepot_Test.java
+++ bootCmpMvn/src/test/java/homeDepotProject_1/homeDepot_Test.java
@@ -2,20 +2,26 @@ package homeDepotProject_1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+
 
 public class homeDepot_Test {
 	
 	WebDriver driver;
 	
-  @Test
-  public void RunhomeDepot() {
+	 @Parameters({"Browser", "URL", "email", "password", "ZipCode", "Phonenumber"})
+	@Test
+	
+  public void RunhomeDepot(String br, String url, String em, String pw, String zip, String phone) {
 
-	 driver = OpenBrowser.openbrowser("chrome", "https://www.homedepot.com/");
+	 driver = OpenBrowser.openbrowser(br, url);
+	 
 		  homeDepotPOM obj = PageFactory.initElements(driver, homeDepotPOM.class);
 		//obj.createAcc(inputEmail, inputpassword, ZipCode, inputphoneNumber);
 		  
-		  obj.createAcc("mesfinkassa@yahoo.com", "mka12345", "22206", "9116001333");
+		  obj.createAcc(em, pw, zip, phone);
 		  
 		  
 		  
