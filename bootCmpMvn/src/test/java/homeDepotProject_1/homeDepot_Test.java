ec15b25720a05a8a69cd1d20a063e6af15291f64
Mesfin Kassa <Mesfin Kassa@DESKTOP-57TO61H>
2019-07-19 19:31:14 -0400

    finale commit

--- bootCmpMvn/src/test/java/homeDepotProject_1/homeDepot_Test.java
+++ bootCmpMvn/src/test/java/homeDepotProject_1/homeDepot_Test.java
@@ -2,6 +2,7 @@ package homeDepotProject_1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,7 @@ public class homeDepot_Test {
 	
 	WebDriver driver;
 	
-	 @Parameters({"Browser", "URL", "email", "password", "ZipCode", "Phonenumber"})
+	@Parameters({"Browser", "URL", "email", "password", "ZipCode", "Phonenumber"})
 	@Test
 	public void RunhomeDepot(String br, String url, String em, String pw, String zip, String phone) {
 
@@ -19,11 +20,13 @@ public class homeDepot_Test {
 	 
 		  homeDepotPOM obj = PageFactory.initElements(driver, homeDepotPOM.class);	  
 		  obj.createAcc(em, pw, zip, phone);
-		   
 		  
 	  }
 	  
-	  
+	  @AfterTest
+	  public void closeBrowser() {
+		  driver.quit();
+	  }
 	  
 	  
   }
